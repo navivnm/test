af696b6156d809434b65baab7bef5db124dd8c5d
Naveen Vijay <naveenvijay@Naveens-Mac.local>
2019-05-26 16:27:03 -0400

    added new print function, unit test working ok.

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -8,17 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    var total = Int()
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        funcPrint(abc: 10)
+    }
+}
+
+extension ViewController
+{
+    func funcPrint(abc: Int)
+    {
+        total = abc + 10
         print("hello")
-        let abc = 10
-        for i in 1...abc {
+        for i in 1...abc
+        {
             print(i)
         }
     }
 }
-
